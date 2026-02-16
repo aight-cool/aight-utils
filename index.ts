3b864e80cce0919a0cd0b391cdaa742e2b64d082
admin <admin@admins-MacBook-Pro.local>
2026-02-15 22:42:40 -0500

    Revert "feat: add aight.shortcuts.parse RPC for shortcut name/emoji extraction"
    This reverts commit c4b323722fa8b4069e3cb2fce4b75e8889deeb86.

--- index.ts
+++ index.ts
@@ -11,7 +11,6 @@ import { registerPush } from "./src/push.js";
 import { registerReminders } from "./src/reminders.js";
 import { registerBootstrap } from "./src/bootstrap.js";
 import { registerPushHook } from "./src/push-hook.js";
-import { registerShortcuts } from "./src/shortcuts.js";
 
 const aightPlugin = {
   id: "aight-utils",
@@ -63,7 +62,6 @@ const aightPlugin = {
     registerReminders(api, cfg);
     registerBootstrap(api);
     registerPushHook(api);
-    registerShortcuts(api);
 
     api.logger.info("[aight-utils] Plugin loaded");
   },
